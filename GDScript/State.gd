103c6653fa38b7353867e4154ff0e14024bdd31f
by.chi <16047937+gitee_by_chi@user.noreply.gitee.com>
2026-07-22 23:10:25 +0800

    fix:修复发现界面懒更新首次更新时机在窗口大小变动前导致异常的bug
    feat:添加是否启用发现页面懒更新的设置

--- GDScript/State.gd
+++ GDScript/State.gd
@@ -5,3 +5,9 @@ var play_ui_mode := 0:
 			play_ui_mode = value
 			GdScriptFunc.flush()
 var default_current_playlist: Array = []
+func _ready() -> void:
+	get_window().size=GdScriptFunc.get_data("Window","Size",Vector2i(1600,1000))
+	if GdScriptFunc.get_data("Options","RememberWindow",true):
+		var pos:Vector2i=GdScriptFunc.get_data("Window","Position",Vector2i(-114514,-114514))
+		if pos!=Vector2i(-114514,-114514):
+			get_window().position=pos
